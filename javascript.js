a763e7db645f46512e43f93092b43f676d347b9c
Gutovp1 <paivaav@gmail.com>
2022-05-08 12:08:03 -0300

    First calculator layout with basic operators

---
+++ javascript.js
@@ -0,0 +1,25 @@
+let displayValue = 0
+
+function clear(){
+    displayValue = 0;
+    return displayValue;
+}
+
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+const display = document.getElementById('display');
+
