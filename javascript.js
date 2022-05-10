2675d130b4fe7c68ff7de7bda064913e53b5b490
Gutovp1 <paivaav@gmail.com>
2022-05-10 14:58:30 -0300

    Fix display overflow. Create AC and equal buttons in js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
-let displayValue = 0
+let displayValue = 0;
 const buttonsArray = ['+','-','*','/'];
+let previousOperand = 0;
+let currentOperand = 0;
+let operator = "";
 
 function clear(){
-    displayValue = 0;
-    return displayValue;
+    disp.textContent = "";
+    previousOperand = 0;
+    currentOperand = 0;
+    operator = "";    
 }
 
 function add(a, b){
@@ -23,21 +28,50 @@ function divide(a,b){
 }
 
 const disp = document.getElementById('display');
-
 const numButtons = document.querySelectorAll('.numBtn');
+const opeButtons = document.querySelectorAll('.opeBtn');
+const equButton = document.querySelector('.equBtn');
+const aclButton = document.querySelector('.aclBtn');
+
+
 numButtons.forEach( btn => btn.addEventListener('click',() => {
-    disp.textContent = btn.textContent;
+    if(operator == "")
+    {
+        disp.textContent += btn.textContent;
+        previousOperand = Number(disp.textContent);
+        console.log(previousOperand);
+    }
+    else 
+    {
+        disp.textContent = disp.textContent.replace(operator,"");
+        disp.textContent += btn.textContent;
+        currentOperand = Number(disp.textContent);
+         
+    }
 }));
 
-const opeButtons = document.querySelectorAll('.opeBtn');
 opeButtons.forEach( btn => btn.addEventListener('click',() => {
     disp.textContent = btn.textContent;
+    operator = btn.textContent;
 }));
 
-window.addEventListener('keydown',(e) => {
-    //if(buttonsArray.includes(e.key))
-    if(buttonsArray.includes(e.key) || Number(e.key))
-        disp.textContent = e.key;
-    console.log((Number(e.key)));
-});
+// equButton.addEventListener('click', () => {
+//     if(currentOperand && previousOperand)
+//         disp.textContent = currentOperand; 
+// });
+
+aclButton.addEventListener('click', () =>  clear() );
+
+// window.addEventListener('keydown',(e) => {
+//     if(buttonsArray.includes(e.key) || Number(e.key))
+//     {
+//         while (!buttonsArray.includes(e.key))
+//         {
+//             disp.textContent += e.key;
+//             console.log(opeButtons);
+//         }
+//         previousOperand = Number(disp.textContent);
+//         console.log(previousOperand);
+//     }
+// });
 
